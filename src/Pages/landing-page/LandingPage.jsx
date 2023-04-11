a9ea9f8fd43a501073ecfbeb0be0742ec87d08fa
unknown <nnvedward@gmail.com>
2023-04-11 23:21:44 +0100

    Spend, send, invest section

--- src/Pages/landing-page/LandingPage.jsx
+++ src/Pages/landing-page/LandingPage.jsx
@@ -16,11 +16,6 @@ const LandingPage = () => {
 					</a>
 				</div>
 				<div className="hero-img-box">
-					{/* <img
-						class="hero-img"
-						src="src/Assets/img/kudaHero.png"
-						alt="Kuda Bank Illustration"
-					/> */}
 					<div className="spin1"></div>
 					<div className="spin1-cover"></div>
 					<div className="spin2"></div>
@@ -46,6 +41,75 @@ const LandingPage = () => {
 							the Kuda app takes care of the rest, prompt interest payout
 							included.
 						</p>
+					</div>
+					<div className="save-img-box">
+						<img class="save-img" src="src/Assets/img/kudaSave.png" alt="" />
+					</div>
+				</section>
+
+				{/* Save section */}
+				<section className="spend">
+					<div className="spend-img-box">
+						<img class="spend-img" src="src/Assets/img/spend.png" alt="" />
+					</div>
+					<div className="spend-text-box">
+						<h3 className="spend-heading">Spend</h3>
+						<p className="spend-description">
+							Kuda gives you more than one way to pay easily, including a widely
+							accepted debit card issued in partnership with Visa, quick web
+							payments direct from your Kuda account, and location-based gift
+							cards for shopping and subscriptions.
+						</p>
+					</div>
+				</section>
+
+				{/* Save section */}
+				<section className="save">
+					<div className="save-text-box">
+						<h3 className="save-heading">Send</h3>
+						<p className="save-description">
+							From international transfers to on-the-spot payments at market
+							stalls, we’ve simplified sending money so you don’t need to think
+							too much about it.
+						</p>
+						<p className="save-description">
+							You can find your friends on Kuda and make unlimited instant
+							transfers free of charge, send money to local banks without
+							breaking a sweat, or fulfil your promises to family across
+							borders.
+						</p>
+					</div>
+					<div className="send-img-box">
+						<img class="save-img" src="src/Assets/img/kudaSave.png" alt="" />
+					</div>
+				</section>
+
+				<section className="spend">
+					<div className="spend-img-box">
+						<img class="spend-img" src="src/Assets/img/spend.png" alt="" />
+					</div>
+					<div className="spend-text-box">
+						<h3 className="spend-heading">Invest</h3>
+						<p className="spend-description">
+							We’re bringing verified global investment options closer to every
+							African with fractional shares on Kuda, giving anyone with a Kuda
+							account the opportunity to grow their money wherever they are in
+							the world.
+						</p>
+					</div>
+				</section>
+
+				<section className="save">
+					<div className="save-text-box">
+						<h3 className="save-heading">We’re always</h3>
+						<h3 className="save-heading">happy to help you.</h3>
+						<p className="save-description">
+							You can chat with us on the app, slide into our DMs, tweet, leave
+							an Instagram comment, send an email or call. However you choose to
+							reach out, there’ll always be a friendly person there to make your
+							life easy.
+						</p>
+						<a href="https://www.instagram.com">Contact us</a>
 					</div>
 					<div className="save-img-box">
 						<img class="save-img" src="src/Assets/img/kudaSave.png" alt="" />
